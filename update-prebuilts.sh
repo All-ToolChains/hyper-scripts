0b919b7b2e8605985b4d890808a0da7ef6eb3057
Joe Maples <frap129@gmail.com>
2016-05-18 15:44:41 -0400

    update: Fix if statements, use static directory

--- update-prebuilts.sh
+++ update-prebuilts.sh
@@ -17,112 +17,116 @@ AE7='SaberMod-X-Linaro-arm-eabi-7.x SaberMod-X-Uber-arm-eabi-7.x Uber-X-Linaro-a
 ALA4='Linaro-X-SaberMod-arm-linux-androideabi-4.9 Linaro-X-Uber-arm-linux-androideabi-4.9 SaberMod-X-Linaro-arm-linux-androideabi-4.9 SaberMod-X-Uber-arm-linux-androideabi-4.9 Uber-X-Linaro-arm-linux-androideabi-4.9 Uber-X-SaberMod-arm-linux-androideabi-4.9'
 ALA5='Linaro-X-SaberMod-arm-linux-androideabi-5.x Linaro-X-Uber-arm-linux-androideabi-5.x SaberMod-X-Linaro-arm-linux-androideabi-5.x SaberMod-X-Uber-arm-linux-androideabi-5.x Uber-X-Linaro-arm-linux-androideabi-5.x Uber-X-SaberMod-arm-linux-androideabi-5.x'
 
+# Enter out directory
+cd ../out
+OUT=$(pwd)
+
 # Push 4.9 arm-eabi Toolchains
 for dir in $AE4; do
-if [ "$dir" = "Linaro-X-SaberMod*" ]; then
+if [[ "$dir" = "Linaro-X-SaberMod"* ]]; then
 branch=LxSM
-elif  [ "$dir" = "Linaro-X-Uber*" ]; then
+elif [[ "$dir" = "Linaro-X-Uber"* ]]; then
 branch=LxU
-elif  [ "$dir" = "SaberMod-X-Linaro*" ]; then
+elif [[ "$dir" = "SaberMod-X-Linaro"* ]]; then
 branch=SMxL
-elif  [ "$dir" = "SaberMod-X-Uber*" ]; then
+elif [[ "$dir" = "SaberMod-X-Uber"* ]]; then
 branch=SMxU
-elif  [ "$dir" = "Uber-X-Linaro*" ]; then
+elif [[ "$dir" = "Uber-X-Linaro"* ]]; then
 branch=UxL
-elif  [ "$dir" = "Uber-X-SaberMod*" ]; then
+elif [[ "$dir" = "Uber-X-SaberMod"* ]]; then
 branch=UxSM
 fi;
-cd ../out/$dir;
+cd $OUT/$dir;
 git init && git add -A && git commit -m "New Build" -s && git checkout -b $branch && git push https://bitbucket.org/hyper-toolchains/arm-eabi-4.9 $branch;
 done;
 
 # Push 5.x arm-eabi Toolchains
 for dir in $AE5; do
-if [ "$dir" = "Linaro-X-SaberMod*" ]; then
+if [[ "$dir" = "Linaro-X-SaberMod"* ]]; then
 branch=LxSM
-elif  [ "$dir" = "Linaro-X-Uber*" ]; then
+elif [[ "$dir" = "Linaro-X-Uber"* ]]; then
 branch=LxU
-elif  [ "$dir" = "SaberMod-X-Linaro*" ]; then
+elif [[ "$dir" = "SaberMod-X-Linaro"* ]]; then
 branch=SMxL
-elif  [ "$dir" = "SaberMod-X-Uber*" ]; then
+elif [[ "$dir" = "SaberMod-X-Uber"* ]]; then
 branch=SMxU
-elif  [ "$dir" = "Uber-X-Linaro*" ]; then
+elif [[ "$dir" = "Uber-X-Linaro"* ]]; then
 branch=UxL
-elif  [ "$dir" = "Uber-X-SaberMod*" ]; then
+elif [[ "$dir" = "Uber-X-SaberMod"* ]]; then
 branch=UxSM
 fi;
-cd ../out/$dir;
+cd $OUT/$dir;
 git init && git add -A && git commit -m "New Build" -s && git checkout -b $branch && git push https://bitbucket.org/hyper-toolchains/arm-eabi-5.x $branch;
 done;
 
 # Push 6.x arm-eabi Toolchains
 for dir in $AE6; do
-if [ "$dir" = "Linaro-X-SaberMod*" ]; then
+if [[ "$dir" = "Linaro-X-SaberMod"* ]]; then
 branch=LxSM
-elif  [ "$dir" = "Linaro-X-Uber*" ]; then
+elif [[ "$dir" = "Linaro-X-Uber"* ]]; then
 branch=LxU
-elif  [ "$dir" = "SaberMod-X-Linaro*" ]; then
+elif [[ "$dir" = "SaberMod-X-Linaro"* ]]; then
 branch=SMxL
-elif  [ "$dir" = "SaberMod-X-Uber*" ]; then
+elif [[ "$dir" = "SaberMod-X-Uber"* ]]; then
 branch=SMxU
-elif  [ "$dir" = "Uber-X-Linaro*" ]; then
+elif [[ "$dir" = "Uber-X-Linaro"* ]]; then
 branch=UxL
-elif  [ "$dir" = "Uber-X-SaberMod*" ]; then
+elif [[ "$dir" = "Uber-X-SaberMod"* ]]; then
 branch=UxSM
 fi;
-cd ../out/$dir;
+cd $OUT/$dir;
 git init && git add -A && git commit -m "New Build" -s && git checkout -b $branch && git push https://bitbucket.org/hyper-toolchains/arm-eabi-6.x $branch;
 done;
 
 # Push 7.x arm-eabi Toolchains
 for dir in $AE7; do
-if  [ "$dir" = "SaberMod-X-Linaro*" ]; then
+if [[ "$dir" = "SaberMod-X-Linaro"* ]]; then
 branch=SMxL
-elif  [ "$dir" = "SaberMod-X-Uber*" ]; then
+elif [[ "$dir" = "SaberMod-X-Uber"* ]]; then
 branch=SMxU
-elif  [ "$dir" = "Uber-X-Linaro*" ]; then
+elif [[ "$dir" = "Uber-X-Linaro"* ]]; then
 branch=UxL
-elif  [ "$dir" = "Uber-X-SaberMod*" ]; then
+elif [[ "$dir" = "Uber-X-SaberMod"* ]]; then
 branch=UxSM
 fi;
-cd ../out/$dir;
+cd $OUT/$dir;
 git init && git add -A && git commit -m "New Build" -s && git checkout -b $branch && git push https://bitbucket.org/hyper-toolchains/arm-eabi-7.x $branch;
 done;
 
 # Push 4.9 arm-linux-androideabi Toolchains
 for dir in $ALA4; do
-if [ "$dir" = "Linaro-X-SaberMod*" ]; then
+if [[ "$dir" = "Linaro-X-SaberMod"* ]]; then
 branch=LxSM
-elif  [ "$dir" = "Linaro-X-Uber*" ]; then
+elif [[ "$dir" = "Linaro-X-Uber"* ]]; then
 branch=LxU
-elif  [ "$dir" = "SaberMod-X-Linaro*" ]; then
+elif [[ "$dir" = "SaberMod-X-Linaro"* ]]; then
 branch=SMxL
-elif  [ "$dir" = "SaberMod-X-Uber*" ]; then
+elif [[ "$dir" = "SaberMod-X-Uber"* ]]; then
 branch=SMxU
-elif  [ "$dir" = "Uber-X-Linaro*" ]; then
+elif [[ "$dir" = "Uber-X-Linaro"* ]]; then
 branch=UxL
-elif  [ "$dir" = "Uber-X-SaberMod*" ]; then
+elif [[ "$dir" = "Uber-X-SaberMod"* ]]; then
 branch=UxSM
 fi;
-cd ../out/$dir;
+cd $OUT/$dir;
 git init && git add -A && git commit -m "New Build" -s && git checkout -b $branch && git push https://bitbucket.org/hyper-toolchains/arm-linux-androideabi-4.9 $branch;
 done;
 
 # Push 5.x arm-linux-androideabi Toolchains
 for dir in $ALA5; do
-if [ "$dir" = "Linaro-X-SaberMod*" ]; then
+if [[ "$dir" = "Linaro-X-SaberMod"* ]]; then
 branch=LxSM
-elif  [ "$dir" = "Linaro-X-Uber*" ]; then
+elif [[ "$dir" = "Linaro-X-Uber"* ]]; then
 branch=LxU
-elif  [ "$dir" = "SaberMod-X-Linaro*" ]; then
+elif [[ "$dir" = "SaberMod-X-Linaro"* ]]; then
 branch=SMxL
-elif  [ "$dir" = "SaberMod-X-Uber*" ]; then
+elif [[ "$dir" = "SaberMod-X-Uber"* ]]; then
 branch=SMxU
-elif  [ "$dir" = "Uber-X-Linaro*" ]; then
+elif [[ "$dir" = "Uber-X-Linaro"* ]]; then
 branch=UxL
-elif  [ "$dir" = "Uber-X-SaberMod*" ]; then
+elif [[ "$dir" = "Uber-X-SaberMod"* ]]; then
 branch=UxSM
 fi;
-cd ../out/$dir;
+cd $OUT/$dir;
 git init && git add -A && git commit -m "New Build" -s && git checkout -b $branch && git push https://bitbucket.org/hyper-toolchains/arm-linux-androideabi-5.x $branch;
 done;
